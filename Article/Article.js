b8e21b16f184e2fc7f85de778782a901f42c84c4
carlsachs <sachscarl@gmail.com>
2020-01-15 15:12:59 -0500

    created elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,14 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function createArticle(title, date, p1, p2, p3){
+
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const date = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+
+
+}
